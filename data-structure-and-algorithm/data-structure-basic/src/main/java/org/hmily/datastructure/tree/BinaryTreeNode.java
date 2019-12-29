646807b1b2eb44b4c0fee6696fce52e0434e8cf1
You-Hmily <706097141@qq.com>
2019-12-29 15:53:15 +0800

    BinaryTreeNode 的简单实现。

--- data-structure-and-algorithm/data-structure-basic/src/main/java/org/hmily/datastructure/tree/BinaryTreeNode.java
+++ data-structure-and-algorithm/data-structure-basic/src/main/java/org/hmily/datastructure/tree/BinaryTreeNode.java
@@ -40,7 +40,42 @@ public class BinaryTreeNode<T> {
     }
 
     public BinaryTreeNode<T> insert(Object o){
-        return  null;
+        BinaryTreeNode<T>  newNode = new BinaryTreeNode<T>((T) o);
+        if (left == null&& right == null){
+            return newNode;
+        }
+        int newHashCode = o.hashCode();
+        if (data.hashCode()> newHashCode){
+            insertLeftTree(left,newNode,newHashCode);
+        }else {
+            insertRightTree(right,newNode,newHashCode);
+        }
+        return this;
+    }
+
+    private BinaryTreeNode<T> insertLeftTree(BinaryTreeNode<T> leftNode,BinaryTreeNode<T> newNode,int newHashCode){
+        if (leftNode==null){
+            leftNode = newNode;
+            return leftNode;
+        }
+        if (leftNode.data.hashCode()>newHashCode){
+            return insertLeftTree(leftNode.left,newNode,newHashCode);
+        }else {
+            return insertRightTree(leftNode.right,newNode,newHashCode);
+        }
+    }
+
+
+    private BinaryTreeNode<T> insertRightTree(BinaryTreeNode<T> rightNode,BinaryTreeNode<T> newNode,int newHashCode){
+        if (rightNode==null){
+            rightNode = newNode;
+            return rightNode;
+        }
+        if (rightNode.data.hashCode()>newHashCode){
+            return insertLeftTree(rightNode.getLeft(),newNode,newHashCode);
+        }else {
+            return insertRightTree(rightNode.getRight(),newNode,newHashCode);
+        }
     }
 
 }
